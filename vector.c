19bcefa9490d9daa324519452f46d65fde9af1e0
Sawyer Maloney <sawyerdmaloney@gmail.com>
2024-04-29 03:32:46 -0400

    made shrink, reorganized resizing, and fixed shrinking float/int casting bug

--- vector.c
+++ vector.c
@@ -17,6 +17,8 @@ typedef _vector * vector;
 
 // forward declarations
 void grow(vector vec);
+void shrink(vector vec);
+void resize(vector vec, int new_capacity);
 
 // malloc wrapper
 void * malloc_checked(int size) {
@@ -50,12 +52,26 @@ void add(vector vec, int a) {
 int pop(vector vec) {
     int removed_element = vec->array[--vec->size];
     vec->array[vec->size] = 0; // may want to change this to be a removed element? By using a struct
-    // TODO resize down for efficiency
+    if (vec->size < vec->capacity / 2) { // do we want any buffer?
+        shrink(vec);
+    }
     return removed_element;    
 }
 
+// wrapper for growing up
 void grow(vector vec) {
     int new_capacity = vec->capacity * RESIZE_RATE;
+    resize(vec, new_capacity);
+}
+
+// wrapper for growing down
+void shrink(vector vec) {
+    int new_capacity = (int)(float)vec->capacity * (1 / (float)RESIZE_RATE);
+    resize(vec, new_capacity);
+}
+
+// main resizing function
+void resize(vector vec, int new_capacity) {
     int * new_array = malloc_checked(sizeof(int) * new_capacity);
     // copy everything over
     for(int i = 0; i < vec->size; i++) {
@@ -73,6 +89,10 @@ int main() {
     for (int i = 0; i < 100; i++) {
         add(vec, i);
     }
-    printf("new size of vec: %d\n", vec->size);
+    printf("new size of vec: %d, new capacity: %d\n", vec->size, vec->capacity);
+    for (int i = 0; i < 30; i++) {
+        printf("popped: %d\n", pop(vec));
+    }
+    printf("size: %d, capacity: %d\n", vec->size, vec->capacity);
     return 1;
 }
