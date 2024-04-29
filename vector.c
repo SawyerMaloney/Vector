9a1f0cc3d9f2f4c1cc0123ba577a885226b49501
Sawyer Maloney <sawyerdmaloney@gmail.com>
2024-04-29 02:59:20 -0400

    adding first code

--- vector.c
+++ vector.c
@@ -1,10 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-struct vector {
-    void ** array;
+// first iteration: assume that we are storing an integer
+
+typedef struct {
+    int * array;
     int size;
-};
+} vector;
+
+vector make_vec() {
+    vector vec;
+    vec.size = 0;
+    return vec;
+}
 
 int main() {
     printf("Hello, World\n");
